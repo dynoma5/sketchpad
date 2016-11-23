8f384b05407987d23424e2e6638e94247ddbaf2c
David Seo <manakete659@gmail.com>
2016-11-22 22:41:32 -0500

    done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
-var numOfBoxes = Math.pow(16,2);	
-	for(i=1; i<numOfBoxes;i++){
-		$("#outline").append("<div class = 'box'></div>")
-		$(".box").css("borderStyle","solid")
-	}
+	var grid = 16;
+createGrid(grid);
+
+$(".box").hover(function(){
+	$(this).css("backgroundColor","black");	
 })
 
+	$("#reset").click(function(){
+		$(".box").css("backgroundColor","white");
+		grid2 = prompt("Enter new grid size",16);
+		createGrid(grid2);
+		$(".box").hover(function(){
+			$(this).css("backgroundColor","black");	
+		})
+	})
+})
+
+function createGrid(number){
+	$("#outline").empty();
+	var numOfBoxes = Math.pow(number,2);	
+	for(i=0; i<numOfBoxes;i++){
+		$("#outline").append("<div class = 'box'></div>")
+	}
+	var length = 800/Math.sqrt(numOfBoxes);
+$(".box").css({"width":length,"height":length});
+}
