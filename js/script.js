81bb9dc3b9a6b9aa2ef232d1df910f45ef7e6281
David Seo <manakete659@gmail.com>
2016-11-23 00:01:20 -0500

    Added a feature button:Rainbow

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	var grid = 16;
+	
 createGrid(grid);
 
 $(".box").hover(function(){
@@ -14,14 +15,23 @@ $(".box").hover(function(){
 			$(this).css("backgroundColor","black");	
 		})
 	})
+	$("#color").click(function(){
+		$(".box").hover(function(){
+			$(this).css("backgroundColor",rand())
+			})
+		
+	})
 })
-
 function createGrid(number){
 	$("#outline").empty();
 	var numOfBoxes = Math.pow(number,2);	
 	for(i=0; i<numOfBoxes;i++){
-		$("#outline").append("<div class = 'box'></div>")
+		$("#outline").append("<div class = 'box'></div>");
 	}
 	var length = 800/Math.sqrt(numOfBoxes);
-$(".box").css({"width":length,"height":length});
+	$(".box").css({"width":length,"height":length});
+}
+function rand() {
+	var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+	return hue;
 }
